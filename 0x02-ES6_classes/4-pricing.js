54fa67e4efca17639cf6bec025d5a3bd7ad45e58
egjallo <ejallow@insistglobal.com>
2023-03-25 17:45:20 +0000

    pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+//import Currency from "./3-currency";
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(val) {
+    this._amount = val;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(val) {
+    this._currency = val;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency._name} (${this.currency._code})`;
+  }
+
+  static convertPrice(amount, conversinRate) {
+    return (amount * conversinRate);
+  }
+}
