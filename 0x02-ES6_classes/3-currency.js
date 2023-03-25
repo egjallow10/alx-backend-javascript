7f277e1e40c9f3651929347c0684480673374911
egjallo <ejallow@insistglobal.com>
2023-03-25 06:11:49 +0000

    currency

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,28 @@
+export default class Currency {
+  constructor(code, name) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') throw new TypeError('name must be a string');
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') throw new TypeError('code must be a string');
+    this._code = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._name}  (${this._code})`;
+  }
+}
