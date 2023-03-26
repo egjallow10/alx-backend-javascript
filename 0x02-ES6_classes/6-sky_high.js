81165460ff2a6def34b2e4a2ed567d01d718b694
egjallo <ejallow@insistglobal.com>
2023-03-26 06:39:13 +0000

    extends

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,20 @@
+import Building from "./5-building";
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floor = floors;
+  }
+
+  get floors() {
+    return this._floor;
+  }
+
+  set floors(val) {
+    this._floor = val;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors.`;
+  }
+}
