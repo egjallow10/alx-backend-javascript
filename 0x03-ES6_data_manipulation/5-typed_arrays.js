403ad4298aff01b4ed414a73bde8f83a77502445
egjallo <ejallow@insistglobal.com>
2023-03-28 22:04:33 +0000

    datatype

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,13 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const view = new Int8Array(buffer);
+  const view = new Int8Array(buffer, 0, length);
 
   if (position >= 0 && position < length) {
-    view[position] = value;
+    // view[position] = value;
+    view.set([value], position);
   } else {
     throw new Error('Position outside range');
   }
 
-  return buffer;
+  return new DataView(buffer);
 }
