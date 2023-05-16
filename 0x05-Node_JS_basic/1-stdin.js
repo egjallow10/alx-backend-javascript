dfb44aa3ac73018a981afedebfb9c70c1e6b4ce6
egjallo <egjallo@insistglobal.com>
2023-05-16 11:57:46 +0000

    stdin and out

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,13 @@
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
+// Using Process stdin
+
+process.stdin.setEncoding('utf8');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (input) => {
+  const name = input.trim();
+  console.log(`Your name is : ${name}`);
 });
 
-readline.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}!`);
-  readline.close();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing');
 });
