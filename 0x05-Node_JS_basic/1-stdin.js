0e37c698211079a98466519b12997fe85f6dc3c0
egjallo <egjallo@insistglobal.com>
2023-05-16 12:13:06 +0000

    stdin and out

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,10 @@
 // Using Process stdin
 
-process.stdin.setEncoding('utf8');
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (input) => {
   const name = input.trim();
-  process.stdout.write(`Your name is : ${name}\n`);
+  process.stdout.write(`Your name is: ${name}\n`);
 });
 
 process.stdin.on('end', () => {
