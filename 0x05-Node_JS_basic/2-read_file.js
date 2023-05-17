694893ac2ddbd14ccb910608deb7e9b26612a354
egjallo <egjallo@insistglobal.com>
2023-05-17 13:58:14 +0000

    task 2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+function countStudents() {
+  try {
+    const data = fs.readFileSync('database.csv', 'utf8');
+    const lines = data.split('\n');
+    const students = lines.map((line) => {
+      const [firstname, lastname, , field] = line.split(',');
+      return { firstname, lastname, field };
+    });
+
+    if (students.length === 0) {
+      throw new Error('Cannot load the database');
+    }
+    const csStudents = students
+      .filter((student) => student.field === 'CS')
+      .map((student) => student.firstname);
+    const sweStudents = students
+      .filter((student) => student.field === 'SWE')
+      .map((student) => student.firstname);
+    console.log(`Number of students: ${students.slice(1).length}`);
+    console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(',')}`);
+    console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+countStudents();
+module.exports = countStudents;
