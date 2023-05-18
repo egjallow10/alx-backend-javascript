8b3406da2443ddd5625249ef3fd0cbc3d0b5e701
egjallo <egjallo@insistglobal.com>
2023-05-18 15:44:44 +0000

    conplex

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -13,8 +13,8 @@ app.get('/students', async (req, res) => {
     const data = await studentDetails(process.argv[2]);
     const { students, sweStudents, csStudents } = data;
     res.write('This is the list of our students');
-    res.write(` Number of students: ${students.length}\n`);
-    res.write(` Number of students in CS: ${csStudents.slice(1).length}. List: ${csStudents.join(', ')}`);
+    res.write(`Number of students: ${students.length}\n`);
+    res.write(`Number of students in CS: ${csStudents.slice(1).length}. List: ${csStudents.join(', ')}\n`);
     res.write(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
     res.end();
   } catch (error) {
