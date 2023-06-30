56a538842b038c7ec036a1c97cd744d0677d0a57
egjallow10 <egjallow10@gmail.com>
2023-06-30 14:53:52 +0000

    Async tests

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,27 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    expect(consoleSpy.calledOnce).to.be.true;
+    consoleSpy.restore();
+  });
+
+  it('sendPaymentRequestToApi with 100 and 20 as args', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+  });
+
+  it('sendPaymentRequestToApi with 10 and 10 as args', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleSpy.calledWith('The total is: 20')).to.be.true;
+  });
+});
