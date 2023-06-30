8401db4c8e42330656d3306d5c4937590b9b0273
egjallow10 <egjallow10@gmail.com>
2023-06-30 11:32:46 +0000

    change the test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,23 +3,11 @@ const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
   it('...', () => {
-    assert.equal(calculateNumber(1.0, 0), 1);
-    assert.equal(calculateNumber(1.3, 0), 1);
-    assert.equal(calculateNumber(1.7, 0), 2);
-  });
-
-  it('...', () => {
-    assert.equal(calculateNumber(0, 1.0), 1);
-    assert.equal(calculateNumber(0, 1.3), 1);
-    assert.equal(calculateNumber(0, 1.7), 2);
-  });
-
-  it('...', () => {
-    assert.equal(calculateNumber(1.3, 0), 1);
-    assert.equal(calculateNumber(0, 1.2), 1);
-    assert.equal(calculateNumber(1.3, 1.3), 2);
-    assert.equal(calculateNumber(1.7, 1.2), 3);
-    assert.equal(calculateNumber(1.3, 1.8), 3);
-    assert.equal(calculateNumber(1.6, 1.8), 4);
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(3.7, 1), 5);
+    assert.strictEqual(calculateNumber(3.7, 1.2), 5);
   });
 });
