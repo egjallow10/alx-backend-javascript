985683b69e14fbbe1f1095699f7673841c97c8ba
egjallow10 <egjallow10@gmail.com>
2023-06-30 13:02:54 +0000

    issues fixed

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,25 +1,22 @@
-const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
+const assert = require('assert');
 
 describe('calculateNumber', () => {
   it('it round the first argument', () => {
-    assert.equal(calculateNumber(1.0, 0), 1);
-    assert.equal(calculateNumber(1.3, 0), 1);
-    assert.equal(calculateNumber(1.7, 0), 2);
+    assert.equal(calculateNumber(15.78, 2), 18);
+    assert.equal(calculateNumber(1.2, 0), 1);
+    assert.equal(calculateNumber(3.5, 2), 6);
   });
 
   it('it round the second argument', () => {
-    assert.equal(calculateNumber(0, 1.0), 1);
-    assert.equal(calculateNumber(0, 1.3), 1);
-    assert.equal(calculateNumber(0, 1.7), 2);
+    assert.equal(calculateNumber(2, 15.78), 18);
+    assert.equal(calculateNumber(0, 1.2), 1);
+    assert.equal(calculateNumber(2, 3.5), 6);
   });
 
-  it('it should return the right number', () => {
-    assert.equal(calculateNumber(1.3, 0), 1);
-    assert.equal(calculateNumber(0, 1.2), 1);
-    assert.equal(calculateNumber(1.3, 1.3), 2);
-    assert.equal(calculateNumber(1.7, 1.2), 3);
-    assert.equal(calculateNumber(1.3, 1.8), 3);
-    assert.equal(calculateNumber(1.6, 1.8), 4);
+  it('add of a and b', () => {
+    assert.equal(calculateNumber(15.78, 2.5), 19);
+    assert.equal(calculateNumber(1.2, 0.2), 1);
+    assert.equal(calculateNumber(3.5, 2.7), 7);
   });
 });
